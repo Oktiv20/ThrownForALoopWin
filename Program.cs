fb542a20dc29c013a0ab34d78ded8fa8680c4ce2
Kyle Blunt <kbluntaudio@gmail.com>
2023-07-14 21:40:22 -0500

    "Initiated ThrownForALoop"

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿List<string> products = new List<string>()
+{
+    "Football",
+    "Hocket Stick",
+    "Boomerang",
+    "Frisbee",
+    "Golf Putter"
+};
+
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+
+Console.WriteLine(greeting);
+
+Console.WriteLine("Products:");
+
+for (int i = 0; i < products.Count; i++)
+{
+    Console.WriteLine($"{i + 1}. {products[i]}");
+}
+
+Console.WriteLine("Please enter a product number:");
+
+int response = int.Parse(Console.ReadLine().Trim());
+
+while (response > 5 || response < 1)
+{
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
+}
+
+Console.WriteLine($"You chose: {response}");
