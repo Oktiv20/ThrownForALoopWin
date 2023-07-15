b1c58d80100c143d60a995ab02657bee79f3b575
Kyle Blunt <kbluntaudio@gmail.com>
2023-07-15 10:29:42 -0500

    implemented doubles and decimals

--- Program.cs
+++ Program.cs
@@ -3,38 +3,42 @@
     new Product()
     {
         Name = "Football",
-        Price = 15,
+        Price = 15.00M,
         Sold = false,
         InStock = true,
         StockDate = new DateTime(2022, 10, 20),
-        ManufactureYear = 2010
+        ManufactureYear = 2010,
+        Condition = 4.2
     },
     new Product()
     {
         Name = "Hockey Stick",
-        Price = 12,
+        Price = 12.00M,
         Sold = false,
         InStock = true,
         StockDate = new DateTime(2022, 10, 20),
-        ManufactureYear = 2010
+        ManufactureYear = 2010,
+        Condition = 3.1
     },
     new Product()
     {
         Name = "Golf Driver",
-        Price = 200,
+        Price = 200.00M,
         Sold = false,
         InStock = true,
         StockDate = new DateTime(2022, 10, 20),
-        ManufactureYear = 2010
+        ManufactureYear = 2010,
+        Condition = 5
     },
     new Product()
     {
         Name = "Baseball",
-        Price = 10,
+        Price = 10.00M,
         Sold = false,
         InStock = true,
         StockDate = new DateTime(2022, 10, 20),
-        ManufactureYear = 2010
+        ManufactureYear = 2010,
+        Condition = 4.5
     },
     
 };
@@ -43,6 +47,16 @@ string greeting = @"Welcome to Thrown For a Loop
 Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
+
+decimal totalValue = 0.0M;
+foreach (Product product in products)
+{
+    if (!product.Sold)
+    {
+        totalValue += product.Price;
+    }
+}
+Console.WriteLine($"Total inventory vlaue: ${totalValue}");
 
 Console.WriteLine("Products:");
 
@@ -70,4 +84,6 @@ TimeSpan timeInStock = now - chosenProduct.StockDate;
 Console.WriteLine(@$"You chose: 
 {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
 It is {now.Year - chosenProduct.ManufactureYear} years old.
-It {(chosenProduct.Sold ? "is not available" : $"has been in stock for {timeInStock.Days} days.")} The {chosenProduct.Name} is currently {(chosenProduct.InStock ? "" : "not ")}in stock.");
+It {(chosenProduct.Sold ? "is not available" : $"has been in stock for {timeInStock.Days} days.")} 
+The {chosenProduct.Name} is currently {(chosenProduct.InStock ? "" : "not ")}in stock.
+It is in {chosenProduct.Condition} condtion.");
